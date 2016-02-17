0416f2b735f72dece559e898808f7a80aae38ae1
jfarnsworth95 <jfarnsworth13@austincollege.edu>
2016-02-17 15:32:08 -0600

    starting code for RecursiveFinder, will need way to interact with puzzle

--- Lab03/src/RecursiveSearch.java
+++ Lab03/src/RecursiveSearch.java
@@ -1,4 +1,21 @@
 
 public class RecursiveSearch {
 
+	public RecursiveSearch(int m, int r, int c,int[] usedSet, Puzzle puzzle){
+		
+		boolean contains;
+		int counter = 0;
+		while(counter < usedSet.length){
+			if(c == usedSet[counter][0] && r == usedSet[counter][1]){
+				contains = true;
+			}
+			else{
+				contains = false;
+			}
+		}
+		
+		if(m <= 0 || r<0 || r > puzzle.length || c<0 || c > puzzle.length || contains == true){
+			
+		}
+	}
 }
