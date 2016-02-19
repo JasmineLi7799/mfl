a60587c423fe40c167b14bcfb58cd5615841e26e
jfarnsworth95 <jfarnsworth13@austincollege.edu>
2016-02-18 21:56:04 -0600

    added isInWordList method to RS, and further worked on main RS search method

--- Lab03/src/StringArrayListMerger.java
+++ Lab03/src/StringArrayListMerger.java
@@ -4,16 +4,25 @@ public class StringArrayListMerger {
 	
 	public ArrayList<String> main;
 	
+	/**
+	 * Constructor
+	 * @param mainArray
+	 */
 	public StringArrayListMerger(ArrayList<String> mainArray){
 		
 		main = mainArray;
 	}
 	
+	/**
+	 * merges two string ArrayLists together 
+	 * @param otherArray
+	 */
 	public void merge(ArrayList<String> otherArray){
 		
 		int counter = 0;
 		while(counter < otherArray.size()){
 			main.add(otherArray.get(counter));
+			counter ++;
 		}
 	}
 
